292d68c0884e8d49ebddd501b1b58fdc8904e80f
Donal D'Silva <x15012743@student.ncirl.ie>
2017-01-28 16:09:35 +0000

    added clearInterval

--- script.js
+++ script.js
@@ -4,9 +4,13 @@ function scriptCheck(){
 
 var seconds = 0;
 
-function timeElapsed(){
-  setInterval(function(){
-    seconds++;
-    document.getElementById("timer").innerHTML = seconds;
-  }, 500);
-}
+function startStop(obj){
+  if (obj.innerHTML == "Start"){
+      var startTimer = setInterval(function(){
+            seconds++;
+            document.getElementById("timer").innerHTML = seconds;}, 500);
+           } else if (obj.innerHTML == "Stop") {
+             clearInterval(startTimer);
+           }
+  }
+
