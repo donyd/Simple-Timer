1bae9388ffcdb6c9422889e6a0b883cb6a656c07
= <=>
2017-01-30 23:18:45 +0000

    fixed clearInterval not responding

--- script.js
+++ script.js
@@ -3,14 +3,16 @@ function scriptCheck(){
 }
 
 var seconds = 0;
+var startTimer;
 
 function startStop(obj){
-  if (obj.innerHTML == "Start"){
-      var startTimer = setInterval(function(){
-            seconds++;
-            document.getElementById("timer").innerHTML = seconds;}, 500);
-           } else if (obj.innerHTML == "Stop") {
-             clearInterval(startTimer);
-           }
+    if (obj.innerHTML == "Start"){
+         startTimer = setInterval(function(){
+              seconds++;
+              document.getElementById("timer").innerHTML = seconds;}, 500);
+              console.log(startTimer);
+    } else if (obj.innerHTML == "Stop") {
+              console.log(startTimer);
+              clearInterval(startTimer);
+      }
   }
-
